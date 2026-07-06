76938fa282f62772f7debd059d02694f47228fd8
Márcio Bulgarelli <bulgarellihnd@gmail.com>
2026-07-06 15:08:50 -0300

    Usa variáveis de ambiente do Supabase

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://bvhgrzurigzzjtrnyknl.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2aGdyenVyaWd6emp0cm55a25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODUwMzEsImV4cCI6MjA5NDM2MTAzMX0.uB6LUCUL8d4hCcMRJOdFM6DDLzmZ0eK2Bc6rH7A2qjI"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
